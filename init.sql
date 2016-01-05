09fbf5536fc334175452c844308e5edcdf5c159e
Jurvis Tan <jurvis.tan@me.com>
2016-01-05 23:10:00 +0800

    update SQL init script

--- init.sql
+++ init.sql
@@ -1,15 +1,21 @@
-CREATE TABLE users (
-  ID INT PRIMARY KEY NOT NULL,
-  email varchar(50) NOT NULL,
-  firstname varchar(50),
-  lastname varchar(50)
+CREATE TABLE users
+(
+    id INTEGER PRIMARY KEY NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    firstname VARCHAR(50),
+    lastname VARCHAR(50)
+);
+CREATE UNIQUE INDEX users_email_uindex ON users (email);
+
+CREATE TABLE records
+(
+    user_id INTEGER NOT NULL,
+    imageurl VARCHAR(100),
+    audiourl VARCHAR(100) NOT NULL,
+    daterecorded TIMESTAMP NOT NULL,
+    location GEOGRAPHY NOT NULL,
+    CONSTRAINT records_pkey PRIMARY KEY (user_id, daterecorded),
+    CONSTRAINT records_user_id_fkey FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
-CREATE TABLE records (
-  user_id INT NOT NULL references users(id),
-  imageUrl varchar(100),
-  audioUrl varchar (100) NOT NULL,
-  dateRecorded timestamp NOT NULL,
-  location geography NOT NULL,
-  PRIMARY KEY (user_id, dateRecorded)
-);
+CREATE EXTENSION postgis;
