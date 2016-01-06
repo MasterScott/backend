bd495f70d640ed56bcbfc8acf049761d339e5f5e
Jurvis Tan <jurvis.tan@me.com>
2016-01-06 11:22:39 +0800

    added configuration options for database

--- model/database.js
+++ model/database.js
@@ -1,4 +1,11 @@
 var pgp = require("pg-promise")(/*options*/);
-var db = pgp("postgresql://localhost:5432/columbus");
+var config = require('../config.json')[process.env.NODE_ENV || 'dev'];
+
+var dbConnString = "postgresql://localhost:5432/columbus";
+if (process.env.NODE_ENV) {
+  dbConnString = "postgres://" + config.db.username + ":" + config.db.password + "@localhost:5432/columbus";
+}
+
+var db = pgp(dbConnString);
 
 module.exports = db;
