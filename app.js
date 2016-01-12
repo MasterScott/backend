90279c326c7233e8b5905bcd12acf63948b1621a
Jurvis Tan <jurvis.tan@me.com>
2016-01-12 11:26:01 +0800

    enable CORS

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var cors = require('cors')
 
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 var users = require('./router/users.js');
 var records = require('./router/records.js');
