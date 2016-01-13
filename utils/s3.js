47f4283e1d6c1b765ddd793d6eec96878ee1f644
Jurvis Tan <jurvis.tan@me.com>
2016-01-13 10:03:38 +0800

    fix bucket name config

--- utils/s3.js
+++ utils/s3.js
@@ -1,4 +1,5 @@
 var AWS = require('aws-sdk');
+var config = require('../config.json')[process.env.NODE_ENV || 'dev']['s3'];
 
 var credentials = new AWS.SharedIniFileCredentials({profile: 'columbus'});
 AWS.config.credentials = credentials;
@@ -7,7 +8,7 @@ var s3 = new AWS.S3();
 
 module.exports = {
   getAsset: function(key) {
-    var params = {Bucket: config.bucketName, Key: key}
+    var params = {Bucket: config.bucketName, Key:key}
     var url = s3.getSignedUrl('getObject', params);
     return url;
   }
