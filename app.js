6b3accedf53a645f89fe7965384a6314d4ab2fd6
Jurvis Tan <jurvis.tan@me.com>
2016-01-14 11:35:35 +0800

    added authorization middleware

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var cors = require('cors')
+var config = require('./config.json')[process.env.NODE_ENV || 'dev'];
 
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+
+var authorization = function(req, res, next){
+  if (process.env.NODE_ENV) {
+    if (req.get('Authorization') === config.auth_key) {
+      res.sendStatus(403);
+    }
+  }
+  next();
+}
+app.use(authorization);
 
 var users = require('./router/users.js');
 var records = require('./router/records.js');
